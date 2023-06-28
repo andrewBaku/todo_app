9b072703af2a35af037c5d35df6f5caba65015e7
andrewBaku <bakuandrej25@gmail.com>
2023-06-28 20:39:57 +0300

    Create functinality to sign-up, log-in and logout. Add this functionality to temlates. Add task only for authenticated users.

--- todo/urls.py
+++ todo/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('user/signup/', views.sign_up, name='sign_up'),
+    path('user/login/', views.log_in, name='log_in'),
+    path('user/logout/', views.log_out, name='logout')
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,28 +1,32 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
 
 from .models import Todo
 from .forms import TodoEnterForm
 
 # Create your views here.
 def homepage(request):
+    todo_tasks = Todo.objects.all()
+    print(request.user.is_authenticated)
     if request.method == "POST":
         form = TodoEnterForm(request.POST)
         if form.is_valid():
-            form.save()
+            print(request)
+            task = form.save(commit=False)
+            task.author = request.user
+            task.save()
             messages.success(request, 'Your Task successfully added!')
-            #return redirect('homepage')
+            return redirect('homepage')
         else:
             messages.error(request, 'Error saving form')
     else:
         form = TodoEnterForm()
-    return render(request, 'todo/homepage.html', {'form': form})
+    return render(request, 'todo/homepage.html', {'form': form, 'todo_tasks': todo_tasks})
 
 def sign_up(request):
-    todo_tasks = Todo.objects.all()
-
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
@@ -34,4 +38,28 @@ def sign_up(request):
             return redirect('homepage')
     else:
         form = UserCreationForm()
-    return render(request, 'todo/sign_up.html', {'form': form, 'todo_tasks': todo_tasks})
+    return render(request, 'todo/sign_up.html', {'form': form})
+
+def log_in(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect('homepage')
+            else:
+                messages.error(request, f"Invalid username or password.")
+        else:
+            messages.error(request, f"Invalid username or password.")
+    else:
+        form = AuthenticationForm()
+    return render(request, 'todo/log_in.html', {'form': form})
+
+def log_out(request):
+    logout(request)
+    messages.info(request, f"You successfully is logout.")
+    return redirect('homepage')
